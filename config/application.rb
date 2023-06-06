82ca784cc0facd3081639b01556e17fd97696565
Cecile Saiz <cecile.saiz@gmail.com>
2023-06-06 15:05:29 +0200

    Changes to language

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module LiliPilates2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    config.i18n.default_locale = :fr
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
