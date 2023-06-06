c7651dbe40ea38bfbc9c09fda2fa8b2adbdb75a1
Laurie Segalowitch <lausegalo@gmail.com>
2023-06-06 14:34:46 +0200

    Routes creation + page reservation

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'bookings/new'
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "dashboard", to: "pages#dashboard"
+  get "reservation", to: "pages#reservation"
+  resources :subscriptions, only: %i[new create]
+  resources :payements
+  resources :courses, only: %i[index]
+  resources :boookings, only: %i[new create destroy]
+  resources :reviews, only: %i[index new create]
 end
