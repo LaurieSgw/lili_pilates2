5e2b1b90aa9f58c71ae39c227fcd35730fac3e55
Cecile Saiz <cecile.saiz@gmail.com>
2023-06-08 17:44:55 +0200

    Added rails admin

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.precompile += %w(application.js)
+Rails.application.config.assets.paths << Rails.root.join("node_modules/@fortawesome/fontawesome-free/webfonts")
