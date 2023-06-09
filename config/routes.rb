390f5058693f68ea714a6a29dc3104b486c47d4d
Laurie Segalowitch <lausegalo@gmail.com>
2023-06-09 15:25:25 +0200

    remove active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  devise_for :admin_users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   devise_for :users
