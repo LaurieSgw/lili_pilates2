c064678da0d00a0229e86245dcf135cb7f97941f
Laurie Segalowitch <lausegalo@gmail.com>
2023-08-11 16:04:06 +0200

    working on stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,10 @@ application.register("hello", HelloController)
 
 import SelectController from "./select_controller"
 application.register("select", SelectController)
+
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
+
+const application = Application.start();
+const context = require.context("controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
--- app/javascript/controllers/select_controller.js
+++ app/javascript/controllers/select_controller.js
@@ -2,22 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="select"
 export default class extends Controller {
-  static targets = [ "abo10", "abo20", "gratuit", "uneseance", "course" ]
+  static targets = ["subscription"];
 
   selection(event) {
-    console.log("helloo");
+    this.subscriptionTargets.forEach(target => {
+      target.classList.remove("selected");
+    });
 
-    event.preventDefault()
-     this.abo10Target.classList.toggle("card-abo-click");
-     this.abo20Target.classList.toggle("card-abo-click");
-     this.gratuitTarget.classList.toggle("card-abo-click");
-     this.uneseanceTarget.classList.toggle("card-abo-click");
-     this.courseTarget.classList.toggle("card-abo-click");
+    event.currentTarget.classList.add("selected");
+}
+  // static targets = [ "abo10", "abo20", "gratuit", "uneseance", "course" ]
 
+  // selection(event) {
+  //   console.log("helloo");
 
-
-
+  //   event.preventDefault()
+  //    this.abo10Target.classList.toggle("card-abo-click");
+  //    this.abo20Target.classList.toggle("card-abo-click");
+  //    this.gratuitTarget.classList.toggle("card-abo-click");
+  //    this.uneseanceTarget.classList.toggle("card-abo-click");
+  //    this.courseTarget.classList.toggle("card-abo-click");
 
 
   }
-}
